b33296bd2cc128624925541dace890d132352820
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-13 23:12:00 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,6 @@
 Module containing the init for the models package
 """
 
-def some_function():
+def initialize_storage():
     from models.engine import file_storage
-
-# Create a variable storage, an instance of FileStorage
-# Call reload() method on this variable
+    storage = file_storage.storage
