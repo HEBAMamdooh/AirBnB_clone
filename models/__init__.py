028ba1c76ae8e5772df4109cba5f535fb60b6b4d
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-13 23:41:22 +0200

    another try

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-Module containing the init for the models package
-"""
-
-def initialize_storage():
-    from models.engine import file_storage
-    storage = file_storage.storage
