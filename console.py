f648acf1209b9a423b2a17bd10a754d8961ff797
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-14 23:49:10 +0200

    Task 6

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Command interpreter for the AirBnB console.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line."""
+        pass
+
+    def help_quit(self):
+        """Print help for quit command."""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Print help for EOF command."""
+        print("EOF command to exit the program")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
