0063547b7115848a798f9703b23f15c03ed29cae
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-16 21:55:28 +0200

    new answer

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,125 +6,65 @@ import unittest
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
-from models import *
 
 
-class TestConsoleFunction(unittest.TestCase):
-    classes = ["User", "State", "Review", "Place", "City", "BaseModel"]
-    actual_obj = None
-    actual_id = ""
-    actual_class = ""
+class TestHBNBCommand(unittest.TestCase):
 
-    def test_create_new_obj(self):
-        if len(storage.all()) != 6:
-            for class_name in self.classes:
-                with patch('sys.stdout', new=StringIO()) as f:
-                    HBNBCommand().onecmd("create {}".format(class_name))
-                    instance_after = len(storage.all())
-                    self.actual_class = class_name
-                    self.actual_id = f.getvalue()[:-1]
-                    key_id = class_name + "." + self.actual_id
-                    self.actual_obj = storage.all()[key_id]
-                    self.assertNotEqual(self.actual_obj, None)
-                    self.assertIn(self.actual_class, self.classes)
+    def setUp(self):
+        self.hbnb_cmd = HBNBCommand()
 
-    def test_count_obj(self):
-        """
-            <class name>.count()
-        """
-        for class_name in self.classes:
-            with patch('sys.stdout', new=StringIO()) as f:
-                HBNBCommand().onecmd("{}.count()".format(class_name))
-                count = 0
-                for key, obj in storage.all().items():
-                    if type(obj) is eval(class_name):
-                        count += 1
-                self.assertEqual(eval(f.getvalue()[:-1]), count)
+    def test_create_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) == 36)  # Assuming the ID is a UUID
 
-    @staticmethod
-    def get_first_occurence_by_class_name(class_name):
-        """
-            :return first occurence found
-        """
+    def test_show_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
 
-        for key, val in storage.all().items():
-            if type(val) is eval(class_name):
-                return val
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd(f"show BaseModel {obj_id}")
+            output = f.getvalue().strip()
+            self.assertTrue(obj_id in output)
 
-    @staticmethod
-    def get_all_obj_by_classname(class_name):
-        """
-        :return all object found in storage
-        """
-        res = []
-        for key, val in storage.all().items():
-            if type(val) is eval(class_name):
-                res.append(str(val))
-        return res
+    def test_destroy_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
 
-    def test_show_obj(self):
-        """
-            <class name>.show(<id>)
-        """
-        for class_name in self.classes:
-            with patch('sys.stdout', new=StringIO()) as f:
-                obj = self.get_first_occurence_by_class_name(class_name)
-                if obj:
-                    HBNBCommand().onecmd("{}.show({})".format(
-                        class_name,
-                        obj.id))
-                    HBNBCommand().onecmd("{}.show({})".format(class_name,
-                                                              obj.id))
-                    self.assertEqual(str(obj), f.getvalue()[:-1])
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd(f"destroy BaseModel {obj_id}")
 
-    def test_all_obj(self):
-        """
-            <class name>.all()
-        """
-        for class_name in self.classes:
-            with patch('sys.stdout', new=StringIO()) as f:
-                all_class_name = self.get_all_obj_by_classname(class_name)
-                HBNBCommand().onecmd("{}.all()".format(class_name))
-                self.assertEqual(all_class_name, eval(f.getvalue()))
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd(f"show BaseModel {obj_id}")
+            output = f.getvalue().strip()
+            self.assertTrue("** no instance found **" in output)
 
-    def test_destroy_obj(self):
-        """
-            <class name>.destroy(<id>)
-        """
+    def test_all_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
 
-    def test_update_obj(self):
-        """
-            <class name>.update(<id>, <attribute name>, <attribute value>)
-        """
-        tests = {"t_int": 120, "t_list": ["id,1", "id,2"],
-                 "t_str": "this is str", "t_float": 5.2}
-        for class_name in self.classes:
-            obj = self.get_first_occurence_by_class_name(class_name)
-            for key, val in tests.items():
-                with patch('sys.stdout', new=StringIO()) as f:
-                    if obj:
-                        HBNBCommand().onecmd(
-                            "{}.update(\"{}\", \"{}\", {})".format(class_name,
-                                                                   obj.id,
-                                                                   key,
-                                                                   val))
-                        newval = getattr(obj, key)
-                        self.assertEqual(type(val), type(newval))
-                        self.assertEqual(val, newval)
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("all BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(obj_id in output)
 
-        tests = {"number_rooms": 120, "amenity_ids": ["id,1", "id,2"],
-                 "description": "this is str", "latitude": 5.2}
-        obj = self.get_first_occurence_by_class_name("Place")
-        for key, val in tests.items():
-            with patch('sys.stdout', new=StringIO()) as f:
-                if obj:
-                    HBNBCommand().onecmd(
-                        "{}.update(\"{}\", \"{}\", {})".format("Place",
-                                                               obj.id,
-                                                               key,
-                                                               val))
-                    newval = getattr(obj, key)
-                    self.assertEqual(type(val), type(newval))
-                    self.assertEqual(val, newval)
-                    """
-                    """
+    def test_update_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd(f"update BaseModel {obj_id} name 'NewName'")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.hbnb_cmd.onecmd(f"show BaseModel {obj_id}")
+            output = f.getvalue().strip()
+            self.assertTrue("NewName" in output)
+
+
+if __name__ == '__main__':
+    unittest.main()
